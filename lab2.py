b878cee40eaf1d01aef749ef0cf17fe3a2cf8274
alex1820alex <amuvie@mail.ru>
2023-06-17 22:50:05 +0300

    строки

--- lab2.py
+++ lab2.py
@@ -1,20 +1,6 @@
 #Напишите программу, которая выполняет следующие математические преобразования:
 import math
-int_1=1**100
-int_2=2**16
-int_3=2-1000
-int_4=2+2*2
-int_5=60/3
-int_6=math.sqrt(144)
-int_7=2+5*14
-int_8=66-11
-int_9=(16-8)**2
-print("int_1:",int_1)
-print("int_2:",int_2)
-print("int_3:",int_3)
-print("int_4:",int_4)
-print("int_5:",int_5)
-print("int_6:",int_6)
-print("int_7:",int_7)
-print("int_8:",int_8)
-print("int_9:",int_9)
+none=None
+str_1="STROKA"*3
+str_2=str(none)+"123"
+print("str_2:",str_2)
