aa535de1d700c315a17f417ea899c4671b0fd48a
alex1820alex <amuvie@mail.ru>
2023-06-18 17:31:29 +0300

    списки

--- lab2.py
+++ lab2.py
@@ -1,10 +1,16 @@
-try:
-    int_1=int(input("Введите высоту треугольника"))
-    for i in range(1,int_1+1):
-        for j in range(i):
-            print("$",end=' ')
-        print()
-
-
-except ValueError:
-    print("Введите высоту треугольника")
+#1. В списке, содержащем положительные и отрицательные целые числа, вычислите сумму чётных положительных элементов.
+#2. Найти в списке те элементы, значение которых меньше среднего арифметического, взятого от всех элементов списка.
+spisok=[1,-2,-3,4,5,6,-7,8,-9]
+summa=0
+for item in spisok:
+    if item>0 and item%2==0:
+        summa+=item
+print(summa)
+summa=0
+for item in spisok:
+    summa+=item
+summa= summa /len(spisok)
+print(summa)
+for item in spisok:
+    if item<summa:
+        print(item)
