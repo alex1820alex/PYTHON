c000cd54205e33dbed3822dba541bddf60ec727e
alex1820alex <amuvie@mail.ru>
2023-06-18 15:51:27 +0300

    сумма цифр

--- lab2.py
+++ lab2.py
@@ -1,24 +1,12 @@
-#Найти сумму элементов, находящихся между минимальным и максимальным элементами.
-# Сами минимальный и максимальный элементы в сумму не включать.
 try:
-    int_1=int(input("Введите число"))
-    minimal=int(str(int_1)[0])
-    maximum=int(str(int_1)[0])
-    id_minimal=0
-    id_maximum=0
+    int_1=int(input("Введите десятизначное число"))
     summa=0
-    shag=1
-    for i in range(len(str(int_1))):
-        if minimal>=int(str(int_1)[i]):
-            minimal=int(str(int_1)[i])
-            id_minimal = i
-        if maximum<=int(str(int_1)[i]):
-            maximum=int(str(int_1)[i])
-            id_maximum = i
-    if id_minimal>id_maximum:
-        shag=-1
-    for i in range(id_minimal+shag,id_maximum,shag):
-        summa+=int(str(int_1)[i])
-    print(summa)
+    if len(str(int_1))<10:
+        for i in range(len(str(int_1))):
+            summa+=int(str(int_1)[i])
+        print(summa)
+    else:
+        print("Введите десятизначное число")
+
 except ValueError:
-    print("Введите  число")
+    print("Введите десятизначное число")
