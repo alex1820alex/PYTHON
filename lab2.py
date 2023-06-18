cb0e16d0863b0ba145eb3be7102c4f2c39ee064e
alex1820alex <amuvie@mail.ru>
2023-06-18 12:12:52 +0300

    Числа и обработчик исключений

--- lab2.py
+++ lab2.py
@@ -1,11 +1,9 @@
-#1. Придумать и обработать исключения на TypeError и ValueError.
-#2. Известны год, номер месяца и день рождения человека, а также день, год и номер месяца сегодняшнего дня. Определите возраст человека (число полных лет).
-from datetime import date
 try:
-    int_1 = int(input("Введите число"))
-    int_1+="adsd"
-except (ValueError,TypeError):
-    print("Ошибка")
-data_peaople = date(2000, 9, 18)
-data_now=date.today()
-print(((data_now-data_peaople)/365).days,"возраст")
+    int_1=int(input("Введите трехзначное число"))
+    if 100<int_1<1000:
+        summa=int(str(int_1)[0])+int(str(int_1)[1])+int(str(int_1)[2])
+        print("Сумма трех цифр числа:",summa)
+    else:
+        print("Не трехзначное число")
+except ValueError:
+    print("Введите число")
