bfcbe7580c6688c00a0f4458c0dee9656c792b45
alex1820alex <amuvie@mail.ru>
2023-06-18 15:12:55 +0300

    совершенные числа

--- lab2.py
+++ lab2.py
@@ -2,23 +2,15 @@
 # Например, если на ввод поступило число 6, то вывод должен содержать шесть первых чисел ряда Фибоначчи: 1 2 3 5 8 13.
 #2. Вычислить факториал введённого числа.
 try:
-    int_1=int(input("Введите количество элементов Фибоначчи"))
-    fib_1 = 0
-    fib_2 = 1
-    i = 0
-    while i < int_1:
-        fib_sum = fib_1 + fib_2
-        print(fib_sum)
-        fib_1 = fib_2
-        fib_2 = fib_sum
-        i +=1
+    int_1=int(input("Введите совершенное число"))
+    sovershen=0
+    for i in range(1,int_1):
+        if(int_1%i==0):
+            sovershen+=i
+    print(sovershen)
+    if sovershen==int_1:
+        print(int_1,"-совершенное число")
+    else:
+        print(int_1, "-не совершенное число")
 except ValueError:
-    print("Введите количество элементов Фибоначчи")
-try:
-    int_1=int(input("Введите число"))
-    factorial=1
-    for i in range(1,int_1+1):
-        factorial*=i
-    print(factorial)
-except ValueError:
-    print("Введите число")
+    print("Введите совершенное число")
