96140fbeea5171efd369490fcfcba2fbf71f89bc
alex1820alex <amuvie@mail.ru>
2023-06-18 15:46:39 +0300

    минмальный и максимальный элемент

--- lab2.py
+++ lab2.py
@@ -1,15 +1,24 @@
-#1. Вывести на экран столько элементов ряда Фибоначчи, сколько указал пользователь.
-# Например, если на ввод поступило число 6, то вывод должен содержать шесть первых чисел ряда Фибоначчи: 1 2 3 5 8 13.
-#2. Вычислить факториал введённого числа.
+#Найти сумму элементов, находящихся между минимальным и максимальным элементами.
+# Сами минимальный и максимальный элементы в сумму не включать.
 try:
     int_1=int(input("Введите число"))
-    prost=0
-    for i in range(1,int_1):
-        prost = 0
-        for j in range(1,i+1):
-            if(i%j==0):
-                prost+=1
-        if prost == 2:
-            print(i)
+    minimal=int(str(int_1)[0])
+    maximum=int(str(int_1)[0])
+    id_minimal=0
+    id_maximum=0
+    summa=0
+    shag=1
+    for i in range(len(str(int_1))):
+        if minimal>=int(str(int_1)[i]):
+            minimal=int(str(int_1)[i])
+            id_minimal = i
+        if maximum<=int(str(int_1)[i]):
+            maximum=int(str(int_1)[i])
+            id_maximum = i
+    if id_minimal>id_maximum:
+        shag=-1
+    for i in range(id_minimal+shag,id_maximum,shag):
+        summa+=int(str(int_1)[i])
+    print(summa)
 except ValueError:
     print("Введите  число")
