2c2ee3e2c01d2139a833da06e9443c5f3eb4f04c
alex1820alex <amuvie@mail.ru>
2023-06-18 16:07:44 +0300

    треугольник

--- lab2.py
+++ lab2.py
@@ -1,12 +1,10 @@
 try:
-    int_1=int(input("Введите десятизначное число"))
-    summa=0
-    if len(str(int_1))<10:
-        for i in range(len(str(int_1))):
-            summa+=int(str(int_1)[i])
-        print(summa)
-    else:
-        print("Введите десятизначное число")
+    int_1=int(input("Введите высоту треугольника"))
+    for i in range(1,int_1+1):
+        for j in range(i):
+            print("$",end=' ')
+        print()
+
 
 except ValueError:
-    print("Введите десятизначное число")
+    print("Введите высоту треугольника")
