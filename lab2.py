d812dbe1a1d5c2ddc80978c80b95fae4f29ed47e
alex1820alex <amuvie@mail.ru>
2023-06-18 18:21:14 +0300

    одномерные массивы

--- lab2.py
+++ lab2.py
@@ -1,16 +1,26 @@
-#1. В списке, содержащем положительные и отрицательные целые числа, вычислите сумму чётных положительных элементов.
-#2. Найти в списке те элементы, значение которых меньше среднего арифметического, взятого от всех элементов списка.
-spisok=[1,-2,-3,4,5,6,-7,8,-9]
-summa=0
-for item in spisok:
-    if item>0 and item%2==0:
-        summa+=item
-print(summa)
-summa=0
-for item in spisok:
-    summa+=item
-summa= summa /len(spisok)
-print(summa)
-for item in spisok:
-    if item<summa:
-        print(item)
+#1. В одномерном массиве целых чисел определить два наименьших элемента.
+# Они могут быть как равны между собой (оба являться минимальными), так и различаться.
+#2. Сжать массив, удалив из него все элементы, величина которых находится в интервале [а, b].
+# Освободившиеся в конце массива элементы заполнить нулями.
+import random
+mas=list()
+for i in range(10):
+    mas.append(random.randint(0,100))
+for item in mas:
+    print(item,end=' ')
+print()
+min_1=1000
+min_2=1000
+for item in mas:
+    if min_1>=item:
+        min_2 = min_1
+        min_1=item
+print(min_1,min_2)
+a=int(input("введите интервал"))
+b=int(input(""))
+for item in mas:
+    if a<=item<=b:
+        mas.pop(mas.index(item))
+        mas.append(0)
+for item in mas:
+    print(item,end=' ')
