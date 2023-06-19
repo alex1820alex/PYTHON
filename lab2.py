0d7789b4b117d96d2b0c55754e162b78640d1310
alex1820alex <amuvie@mail.ru>
2023-06-19 23:04:59 +0300

    множества

--- lab2.py
+++ lab2.py
@@ -1,12 +1,25 @@
-#Дана строка. Сохранить в ней только первые вхождения символов, удалив все остальные.
-# Результат вывести в виде кортежа.
-str_1="Hello World"
-str_2=str_1
-kortej=()
-for i in range(len(str_1)):
-    if str_1[i] not in kortej:
-        kortej+=(str_1[i],)
-
+#1. Дано 2 множества, содержащих названия IT-компаний.
+# Найти только те компании, которые содержатся в обоих множествах.
+#2. Сгенерировать n множеств. Вывести множества кратные трём и не входящие в первое множество.
+import random
+mnojistva_1={"Лидер Кодинг","MexiNext","КодЭкспресс","Black Fox Testers"}
+mnojistva_2={"HardPlay","Creative Codes","Magma Technology","MexiNext"}
+print(mnojistva_1 & mnojistva_2)
+kortej=list()
+n=int(input("Введите кл-во множеств"))
+for i in range(n):
+    k=random.randint(1,3)
+    mnojistva = set()
+    for j in range(k):
+        mnojistva.add(random.randint(0,5))
+    kortej.append(mnojistva)
 print(kortej)
-
+for item in (kortej):
+    if len(item)%3==0:
+        for i in kortej:
+            if i.issuperset(item) and i!=item:
+                break
+            else:
+                print(item)
+                break
 
