0f453ff5278711e0a3a3bee36a819023993af123
alex1820alex <amuvie@mail.ru>
2023-06-19 12:26:54 +0300

    матрица

--- lab2.py
+++ lab2.py
@@ -1,26 +1,25 @@
-#1. В одномерном массиве целых чисел определить два наименьших элемента.
-# Они могут быть как равны между собой (оба являться минимальными), так и различаться.
-#2. Сжать массив, удалив из него все элементы, величина которых находится в интервале [а, b].
-# Освободившиеся в конце массива элементы заполнить нулями.
+#1. Найти максимальный элемент диагонали матрицы.
+#2. Вычислить количество отрицательных элементов под главной диагональю матрицы.
 import random
-mas=list()
-for i in range(10):
-    mas.append(random.randint(0,100))
+m=3
+n=3
+mas=[]
+maximum=-100
+for i in range(n):
+    mas_0=[]
+    for j in range(m):
+        mas_0.append(random.randint(-100,100))
+    mas.append(mas_0)
 for item in mas:
-    print(item,end=' ')
-print()
-min_1=1000
-min_2=1000
-for item in mas:
-    if min_1>=item:
-        min_2 = min_1
-        min_1=item
-print(min_1,min_2)
-a=int(input("введите интервал"))
-b=int(input(""))
-for item in mas:
-    if a<=item<=b:
-        mas.pop(mas.index(item))
-        mas.append(0)
-for item in mas:
-    print(item,end=' ')
+    print(item)
+for i in range(n):
+    for j in range(m):
+        if j == i and maximum < mas[i][j]:
+            maximum = mas[i][j]
+print(maximum)
+coust=0
+for i in range(n):
+    for j in range(m):
+        if j < i and mas[i][j]<0:
+            coust+=1
+print(coust)
