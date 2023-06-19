9230ce3476e80a96de17899a48fbf906ded975d5
alex1820alex <amuvie@mail.ru>
2023-06-19 23:45:14 +0300

    словарь

--- lab2.py
+++ lab2.py
@@ -1,6 +1,17 @@
-str_1=input("Введите строку")
-spisok=list(str_1.split(" "))
-print(spisok)
-mnojestva=set(spisok)
-spisok=list(mnojestva)
-print(sorted(spisok))
+slovar={
+    "dog":"sobaka",
+    "cat":"koshaka",
+    "home":"dom"
+}
+str_1=input("Введите слово")
+for key,value in slovar.items():
+    if str_1==key:
+        print(value)
+        break
+    elif str_1==value:
+        print(key)
+        break
+else:
+    print("Перевода нет")
+
+
