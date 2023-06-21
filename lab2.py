52511d4d757adc3a776f0f0ab10df6a27d5aed00
alex1820alex <amuvie@mail.ru>
2023-06-21 23:02:07 +0300

    калькулятор

--- lab2.py
+++ lab2.py
@@ -1,9 +1,36 @@
 #Написать функцию, которая определяет количество разрядов введённого целого числа.
-def razryad(int1):
-    razryad_chisla=0
-    while int1>1:
-        int1=int1/10
-        razryad_chisla+=1
-    return razryad_chisla
-int1=int(input("Введите число"))
-print("Разряд числа=",razryad(int1))
+def plus(chislo):
+    chislo2=int(input("Введите второе число"))
+    rezult=chislo+chislo2
+    return rezult
+def minus(chislo):
+    chislo2 = int(input("Введите второе число"))
+    rezult = chislo - chislo2
+    return rezult
+def umnojenie(chislo):
+    chislo2 = int(input("Введите второе число"))
+    rezult = chislo * chislo2
+    return rezult
+def delenie(chislo):
+    chislo2 = int(input("Введите второе число"))
+    rezult = chislo / chislo2
+    return rezult
+rezult=int(input("Введите число:"))
+while rezult:
+    str1=input("Введите +,-,*,/")
+    if str1=="+":
+        rezult=plus(rezult)
+        print("Результат:",rezult)
+    elif str1=="-":
+        rezult=minus(rezult)
+        print("Результат:", rezult)
+    elif str1=="*":
+        rezult=umnojenie(rezult)
+        print("Результат:", rezult)
+    elif str1=="/":
+        rezult=delenie(rezult)
+        print("Результат:", rezult)
+    else:
+        break
+
+
