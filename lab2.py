464eb21a37fa1a21397436c4f27a9a33ac070ea2
alex1820alex <amuvie@mail.ru>
2023-06-21 22:49:43 +0300

    разряд числа

--- lab2.py
+++ lab2.py
@@ -1,17 +1,9 @@
-def generait_derict(str1):
-    mnojestva=set()
-    for i in range(len(str1)):
-        mnojestva.add(str1[i])
-    key=0
-    derict=dict()
-    for item in mnojestva:
-        for i in range(len(str1)):
-            if item==str1[i]:
-                key+=1
-        derict[item]=key
-        key = 0
-    for key,val in derict.items():
-        print(key,val,end=" ")
-
-str1=input("Введите строку")
-generait_derict(str1)
+#Написать функцию, которая определяет количество разрядов введённого целого числа.
+def razryad(int1):
+    razryad_chisla=0
+    while int1>1:
+        int1=int1/10
+        razryad_chisla+=1
+    return razryad_chisla
+int1=int(input("Введите число"))
+print("Разряд числа=",razryad(int1))
