d55fbdec2a5984bfcde57f42844ececa85b4f108
alex1820alex <amuvie@mail.ru>
2023-06-22 13:31:52 +0300

    рекурсия

--- lab2.py
+++ lab2.py
@@ -1,20 +1,8 @@
-from kalkulyator.pacet2 import func2, func3
-from kalkulyator.pacet1 import func1
-
-rezult=int(input("Введите число:"))
-while rezult:
-    str1=input("Введите +,-,*,/")
-    if str1=="+":
-        rezult= func1.plus(rezult)
-        print("Результат:",rezult)
-    elif str1=="-":
-        rezult= func2.minus(rezult)
-        print("Результат:", rezult)
-    elif str1=="*":
-        rezult= func3.umnojenie(rezult)
-        print("Результат:", rezult)
-    elif str1=="/":
-        rezult= func3.delenie(rezult)
-        print("Результат:", rezult)
+x=int(input())
+y=int(input())
+def recurs(x,y):
+    if y>0:
+        return x*recurs(x,y-1)
     else:
-        break
+        return 1
+print(recurs(x,y))
