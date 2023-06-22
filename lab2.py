b5971344d0da5ac0cdcce9f48dc6bd79b06b38a6
alex1820alex <amuvie@mail.ru>
2023-06-22 23:14:41 +0300

    классы

--- lab2.py
+++ lab2.py
@@ -1,12 +1,13 @@
-#Написать функцию, которая просит ввести имя и выводит на экран "Привет и введённое имя".
-#Далее написать к функции декоратор, который изменяет функцию и переводит имя в заглавные буквы.
-def out(func):
-    name = input("Введите имя")
-    print("Привет ",name )
-    def warrer():
-        func(name)
-    return warrer
-@out
-def Name(name):
-    print("Привет ",name.upper())
-Name()
+#Создать класс и вызвать пять объектов этого класса.
+class peaple():
+    def __init__(self,age,first_name,last_name,toll,sex):
+        self.age=age
+        self.first_name=first_name
+        self.last_name = last_name
+        self.toll=toll
+        self.sex=sex
+
+    def hello(self):
+        print("Привет", self.first_name,self.last_name,"ваш возраст",self.age,"рост",self.toll,"пол",self.sex)
+human1=peaple(26,"Никита","Петроясян","173см","мужчина")
+human1.hello()
