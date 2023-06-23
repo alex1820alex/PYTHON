6cc0788fb038acee61fbc1d0ceee339f84495186
alex1820alex <amuvie@mail.ru>
2023-06-23 14:29:38 +0300

    классы наследование

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ import random
 
 
 class animal():
-    def __init__(self,name,age,sex,):
+    def __init__(self,name,age,sex):
         self.name=name
         self.age=age
         self.sex=sex
@@ -13,3 +13,21 @@ class animal():
 
     def massa(self):
         print(self.name,random.randint(30,60),"кг")
+class Wolf(animal):
+    def __init__(self,name,age,sex,kol):
+        super().__init__(name,age,sex)
+        self.kol=kol
+    def staya(self):
+        print("Количество особей в стае",self.kol)
+class Dog(animal):
+    def __init__(self,name,age,sex):
+        super().__init__(name,age,sex)
+
+    def run(self):
+        print("Собака бежит")
+class Cat(animal):
+    def __init__(self, name, age, sex):
+        super().__init__(name, age, sex)
+
+    def sleep(self):
+        print("Кот спит")
--- lab2.py
+++ lab2.py
@@ -1,6 +1,16 @@
 #Создайте класс на тему животных. В классе должен присутствовать конструктор не менее трёх методов.
-import classes
-wolf=classes.animal("wolf",7,"мужской")
+from classes import animal,Wolf,Cat,Dog
+wolf=Wolf("wolf",7,"мужской",5)
+cat=Cat("cat",9,"женский")
+dog=Dog("dog",3,"мужской")
 wolf.info()
 wolf.move(55)
 wolf.massa()
+wolf.staya()
+cat.info()
+cat.sleep()
+dog.info()
+dog.run()
+
+
+
