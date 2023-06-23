5098aa10b7c64d28cfff66cf9cce99fb1743c866
alex1820alex <amuvie@mail.ru>
2023-06-23 20:05:10 +0300

    sqllite создание БД

--- lab2.py
+++ lab2.py
@@ -1,14 +1,21 @@
-#В наследниках класса из предыдущего урока переопределить методы из класса родителя,
-#а также добавить не менее трёх значений по умолчанию.
-from classes import animal,Wolf,Cat,Dog
-wolf=Wolf("wolf",7,"мужской")
-cat=Cat("cat",9,"женский")
-dog=Dog("dog",3,"мужской")
-wolf._info()
-dog1=Dog()
-dog1._info()
-wolf._Wolf__massa()
-cat1=Cat(name="Britanec",sex="женский")
-cat1._info()
+import sqlite3
+coon=sqlite3.connect("firmi.db")
+curs=coon.cursor()
+
+zap1="""CREATE TABLE SPRAVOCHNIK (F_I_O TEXT,YCHINAYA_Stepen TEXT,SINCE_WAY TEXT,MESTO_RABOTI TEXT,
+        KAFEDRA TEXT, DOLJNOST TEXT,CONTRY TEXT,CITY TEXT,ADRESS TEXT,PHONE TEXT,MAIL TEXT);"""
+zap2="""CREATE TABLE INFO (DOKLADCHIK BOOL,PRIGLASHENIE DATE,TEMA_DOKLADA TEXT,TEZIS TEXT,
+        PRIEZD DATE,GOSTINICA BOOL,OTYEZD DATE);"""
 
 
+zap3="""CREATE TABLE CONFIRENCYA (NAME_CONFIRENCYA TEXT,PROVIDENIYE DATE,MESTO TEXT,ORGONIZATORS TEXT,SPONSORS TEXT,KOLICHESTVO_DAYS INTEGER,
+        KOL_PLAYERS INTEGER,KOL_DOKLADCHIKOV INTEGER);"""
+
+curs.execute(zap1)
+curs.execute(zap2)
+curs.execute(zap3)
+
+coon.commit()
+
+curs.close()
+coon.close()
